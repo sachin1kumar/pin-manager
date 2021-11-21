b94b4de014ab9db5828c37ee2898a9c5caba85ee
Sachin <sachinkmr375@gmail.com>
2021-11-21 19:10:18 +0530

    init repo

---
+++ src/main/java/com/pin/manager/pinmanager/PinManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pin.manager.pinmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pin/manager/pinmanager/PinManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pin.manager.pinmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
